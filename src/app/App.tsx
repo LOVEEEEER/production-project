061fe23b4b4c4c2ad40c1498d8c47786160ef3c2
Nikita Demyanenko <nikitademyanenko000@gmail.com>
2023-10-10 02:10:39 +1000

    feat: add navbar

---
+++ src/app/App.tsx
@@ -0,0 +1,19 @@
+import "./styles/index.scss";
+import { useTheme } from "app/providers/ThemeProvider";
+import { AppRouter } from "./providers/router";
+import { classNames } from "shared/lib/classNames/classNames";
+import { Navbar } from "widgets/Navbar";
+
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <Navbar />
+      <AppRouter />
+      <button onClick={toggleTheme}>TOGGLE</button>
+    </div>
+  );
+};
+
+export default App;
