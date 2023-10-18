21750aef27e64ebfcbc9d1b36afbc3eff09053f1
Nikita Demyanenko <nikitademyanenko000@gmail.com>
2023-10-18 19:40:52 +1000

    feat: add eslint, fix lint errors in all files

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,24 @@
-import "./styles/index.scss";
-import { useTheme } from "app/providers/ThemeProvider";
-import { AppRouter } from "./providers/router";
-import { classNames } from "shared/lib/classNames/classNames";
-import { Navbar } from "widgets/Navbar";
+import { Suspense } from 'react';
+import './styles/index.scss';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+import { AppRouter } from './providers/router';
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme();
-
-  return (
-    <div className={classNames("app", {}, [theme])}>
-      <Navbar />
-      <AppRouter />
-      <button onClick={toggleTheme}>TOGGLE</button>
-    </div>
-  );
+    const { theme } = useTheme();
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
+        </div>
+    );
 };
 
 export default App;
--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -1,7 +1,7 @@
-import {classNames} from "shared/lib/classNames/classNames";
-import cls from "./AppLink.module.scss";
-import {Link, LinkProps} from "react-router-dom";
-import {FC, PropsWithChildren} from "react";
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Link, LinkProps } from 'react-router-dom';
+import { FC, PropsWithChildren } from 'react';
+import cls from './AppLink.module.scss';
 
 export enum AppLinkTheme {
     PRIMARY = 'primary',
@@ -14,21 +14,22 @@ interface AppLinkProps extends LinkProps {
     theme?: AppLinkTheme
 }
 
-
 export const AppLink: FC<PropsWithChildren<AppLinkProps>> = (props) => {
-    const {to,
+    const {
+        to,
         className,
         children,
         theme = AppLinkTheme.PRIMARY,
         ...rest
-    } = props
+    } = props;
 
-
-    return <Link
-        className={classNames(cls.AppLink, {}, [className, cls[theme]])}
-        to={to}
-        {...rest}
-    >
-        {children}
-    </Link>
-}
+    return (
+        <Link
+            className={classNames(cls.AppLink, {}, [className, cls[theme]])}
+            to={to}
+            {...rest}
+        >
+            {children}
+        </Link>
+    );
+};
